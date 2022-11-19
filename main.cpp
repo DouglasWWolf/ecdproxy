b4b8bb6005d129d0bc2c542040665ae208e1310d
DouglasWWolf <douglaswolf@yahoo.com>
2022-11-18 18:42:48 -0800

    No change history kept

--- main.cpp
+++ main.cpp
@@ -11,10 +11,17 @@
 
 using namespace std;
 
+// This is the geometry of a single block.  Right now, one block = 1 row of data
+const uint32_t BYTES_PER_CYCLE = 32;
+const uint32_t CYCLES_PER_BLOCK = 64;
+
+// At 32 bytes-per-cycle and 64 cycles-per-block, this works out to be exactly 2K (2048 bytes)
+const uint32_t BYTES_PER_BLOCK = BYTES_PER_CYCLE * CYCLES_PER_BLOCK;
+
 // The addresses and size of the ping-pong buffers
 const uint64_t PPB0 = 0x100000000;   // Address 4G
 const uint64_t PPB1 = 0x200000000;   // Address 8G
-const uint32_t PPB_BLOCKS = 0x100000000LL / 2048;  // However many rows will fit into 4G
+const uint32_t PPB_BLOCKS = 0x20000000 / BYTES_PER_BLOCK;  // However many rows will fit into 512MB
 
 // Userspace pointer to reserved RAM
 uint8_t* physMem;
@@ -137,10 +144,16 @@ void execute()
 
     // Query the RTL design for revision information and display it
     string version = proxy.getMasterBitstreamVersion();
-    cout << "Version is " << version << "\n";
+    cout << "RTL version is " << version << "\n";
     string date = proxy.getMasterBitstreamDate();
-    cout << "Date: " << date << "\n";
+    cout << "RTL date: " << date << "\n";
 
+    // Check to make sure that both QSFP channels are up
+    proxy.checkQsfpStatus(0, true);
+    cout << "QSFP Channel 0 is up\n";
+    proxy.checkQsfpStatus(1, true);
+    cout << "QSFP Channel 1 is up\n";
+    
     // Fill the ping-pong buffers
     fillBuffer(0, 0);
     fillBuffer(1, 0);
@@ -201,6 +214,12 @@ void ECD::onInterrupt(int irq, uint64_t irqCounter)
     // printf for demonstration purposes.  This is impractical in a real application
     printf("Servicing IRQ %i, #%lu\n", irq, irqCounter);
     
+    /*
+     *
+     *     In real life, we would refill the ping-pong buffer here
+     * 
+     */
+
     // Notify the ECD-Master that this buffer has been refilled
     notifyBufferFull(irq);
 }
@@ -208,11 +227,28 @@ void ECD::onInterrupt(int irq, uint64_t irqCounter)
 
 
 //=================================================================================================
-// fillBuffer() - This stuffs some data into the buffer for the purposes of our demo
+// fillBuffer() - This stuffs some data into the DMA buffer for the purposes of our demo
+//
+//                          <<< THIS ROUTINE IS A KLUDGE >>>
+//
+// Because of yet unresolved issues with very slow-writes to the DMA buffer, we are reading the
+// file into a local user-space buffer then copying it into the DMA buffer.    For reasons we don't
+// yet understand, the MMU allows us to copy a user-space buffer into the DMA space buffer faster
+// than it allows us to write to it directly.
+//
+//                               <<< THIS IS A HACK >>>
+//
+// The hack will be fixed when we figure out how to write a device driver that can allocate
+// very large contiguouis blocks.
+//   
 //=================================================================================================
 void fillBuffer(int which, uint32_t row)
 {
-    printf("Filling buffer #%i\n", which);
+    // One gigabyte
+    const uint32_t ONE_GB = 0x40000000;
+
+    // Tell the user what's taking so long...
+    printf("Loading ping-pong buffer #%i\n", which);
 
     // The offset into our contiguous buffer depends on which ping-pong buffer we're filling
     uint64_t memOffset = (which == 0) ? 0 : (PPB1 - PPB0);
@@ -228,8 +264,11 @@ void fillBuffer(int which, uint32_t row)
         exit(1);
     }
 
-    // A row if data is 2048 bytes.  Compute how many bytes of data to load...
-    uint64_t bytes_remaining = PPB_BLOCKS * 2048;
+    // Allocate a 1GB RAM buffer in userspace
+    uint8_t* local_buffer = new uint8_t[ONE_GB];
+
+    // Compute how many bytes of data to load...
+    uint64_t bytes_remaining = (uint64_t)PPB_BLOCKS * (uint64_t)BYTES_PER_BLOCK;
 
     // While there is still data to load from the file...
     while (bytes_remaining)
@@ -238,16 +277,18 @@ void fillBuffer(int which, uint32_t row)
         size_t block_size = bytes_remaining;
 
         // We're going to load this file in chunks of no more than 1 GB
-        if (block_size > 0x40000000) block_size = 0x40000000;
+        if (block_size > ONE_GB) block_size = ONE_GB;
 
-        // Read in this chunk of the file
-        printf("Loading %lu\n", block_size);
-        size_t rc = read(fd, ptr, block_size);
+        // Load this chunk of the file into our local user-space buffer
+        size_t rc = read(fd, local_buffer, block_size);
         if (rc != block_size)
         {
             perror("read");
             exit(1);
         }
+
+        // Copy the userspace buffer into the contiguous block of physical RAM
+        memcpy(ptr, local_buffer, block_size);
 
         // Bump the pointer to where the next chunk will be stored
         ptr += block_size;
@@ -256,13 +297,10 @@ void fillBuffer(int which, uint32_t row)
         bytes_remaining -= block_size;
     }
 
+    // Free up the local_buffer so we don't leak memory
+    delete[] local_buffer;
+
     // We're done with the input file
     close(fd);
-
-    // We display a "done" message so we can physically see how long
-    // it takes to fill the buffer with data
-    printf("filling buffer #%i complete\n", which);
-
 }
 //=================================================================================================
-
